4d4681c7bcf324f4702ff0351d97c5bdc6943d67
jibeknk <jibekkarieva@gmail.com>
2021-04-05 09:28:55 +0600

    added second test page and url

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
+
 def homepage(request):
     return HttpResponse("hello world!")
 
+
 def test(request):
     return render(request, "test.html")
+
+
+def second(request):
+    return HttpResponse("test 2 page")
