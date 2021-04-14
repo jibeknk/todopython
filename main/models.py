afe6c139db071c58cadf9f4ad65e4c7c85b73d70
jibeknk <jibekkarieva@gmail.com>
2021-04-14 16:54:24 +0600

    added book deletion and book marking

--- main/models.py
+++ main/models.py
@@ -16,5 +16,6 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     year = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_favorite = models.BooleanField(default=False)
 
 
